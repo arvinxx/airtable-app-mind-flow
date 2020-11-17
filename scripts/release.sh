cc09e6d2ff1d8b8420f3504f2972e325c4e9c90b
arvinxx <arvinx@foxmail.com>
2020-11-17 11:04:59 +0800

    :wrench: chore: 更新发布模板

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,10 @@
  block release
+ echo '🚀 开发百宝袋 发布完毕!'
  block release --remote dev-project
+ echo '🚀 开发项目 发布完毕!'
  block release --remote academic
+ echo '🚀 学术1on1 发布完毕!'
  block release --remote design-thinking
- block release --remote yunchou
+ echo '🚀 设计思维 发布完毕!'
+ block release --remote domain
+ echo '🚀 领域知识 发布完毕!'
